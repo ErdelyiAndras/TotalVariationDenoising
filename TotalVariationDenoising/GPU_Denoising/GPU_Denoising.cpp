e313f4556387151568e9e8667cfbf060565c12cd
ErdelyiAndras <andraserdelyi17@gmail.com>
2025-06-23 22:40:06 +0200

    added suppress log to command line arguments

--- TotalVariationDenoising/GPU_Denoising/GPU_Denoising.cpp
+++ TotalVariationDenoising/GPU_Denoising/GPU_Denoising.cpp
@@ -3,16 +3,23 @@
 
 #include <CL/cl.hpp>
 #include <oclutils.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
 #include <string>
 #include "../Image/Image.h"
 #include "Denoising.h"
 
 int main(int argc, char** argv) {
-	if (argc != 6) {
+	if (argc != 7) {
 		std::cerr << "Usage: " << argv[0] 
-			      << " <input_image_path> <output_image_path> <strength> <step_size> <tol>" 
+			      << " <input_image_path> <output_image_path> <strength> <step_size> <tol> <suppress_log>" 
 			      << std::endl;
 		return -1;
+	}
+	std::string suppress_log_str = argv[6];
+	bool suppress_log = true;
+	if (suppress_log_str == "false" || suppress_log_str == "0") {
+		suppress_log = false;
 	}
 
 	try {
@@ -27,7 +34,19 @@ int main(int argc, char** argv) {
 		cl::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
 		cl::CommandQueue queue(context, devices[0], CL_QUEUE_PROFILING_ENABLE);
 
-		std::string source_code = oclReadSourcesFromFile("Denoising.cl");
+
+		std::string kernel_path;
+		char* value = nullptr;
+		size_t len = 0;
+		if (_dupenv_s(&value, &len, "DENOISING_KERNEL_PATH") == 0 && value != nullptr) {
+			kernel_path = value;
+			free(value);
+		}
+		else {
+			std::cerr << "Environment variable not set." << std::endl;
+			return -1;
+		}
+		std::string source_code = oclReadSourcesFromFile(kernel_path.c_str());
 		cl::Program::Sources sources(1, std::make_pair(source_code.c_str(), source_code.length() + 1));
 
 		cl::Program program(context, sources);
@@ -49,7 +68,7 @@ int main(int argc, char** argv) {
 
 		auto start = std::chrono::high_resolution_clock::now();
 
-		Image denoisedImage = tv_denoise_gradient_descent(context, queue, program, image, strength, step_size, tol);
+		Image denoisedImage = tv_denoise_gradient_descent(context, queue, program, image, strength, step_size, tol, suppress_log);
 
 		auto end = std::chrono::high_resolution_clock::now();
 
@@ -57,9 +76,11 @@ int main(int argc, char** argv) {
 		std::cout << "GPU_Denoising took: " << elapsed.count() << " seconds" << std::endl;
 
 		cv::Mat displayImage = denoisedImage.toMat();
-
-		cv::imshow("Denoised", displayImage);
-		cv::waitKey(0);
+		
+		if (!suppress_log) {
+			cv::imshow("Denoised", displayImage);
+			cv::waitKey(0);
+		}
 
 		std::string path = argv[2];
 		cv::imwrite(path, displayImage);
