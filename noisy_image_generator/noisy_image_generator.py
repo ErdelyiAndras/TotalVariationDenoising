622fe987a520ba8aa4b1277157f295fd2d1a1cf8
ErdelyiAndras <andraserdelyi17@gmail.com>
2025-06-23 23:53:09 +0200

    changed noisy image generator to only support grayscale images

--- noisy_image_generator/noisy_image_generator.py
+++ noisy_image_generator/noisy_image_generator.py
@@ -2,12 +2,12 @@ from PIL import Image
 import numpy as np
 
 def add_noise(input_path, output_path, noise_std = 25):
-    img = Image.open(input_path).convert('RGB')
+    img = Image.open(input_path).convert('L')
     arr = np.array(img).astype(np.float32)
     
     noise = np.random.normal(0, noise_std, arr.shape)
     noisy_arr = arr + noise
     noisy_arr = np.clip(noisy_arr, 0, 255).astype(np.uint8)
     
-    noisy_img = Image.fromarray(noisy_arr)
+    noisy_img = Image.fromarray(noisy_arr, mode = 'L')
     noisy_img.save(output_path)
